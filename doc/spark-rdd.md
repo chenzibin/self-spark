00cdb70b59316f7d65a5d0022fdc6ea5619ed454
chenzibin <7657838@qq.com>
2022-06-02 18:02:15 +0800

    结构调整

--- doc/spark-rdd.md
+++ doc/spark-rdd.md
@@ -1,3 +1,3 @@
-####rdd缓存
+#### rdd缓存
     1、手动调用cache\present\checkpoint
     2、每次shuffle时，Spark会自动缓存
--- self-spark-demo/src/test/scala/test.scala
+++
@@ -1,8 +0,0 @@
-import java.util.Random
-
-println("this is a")
-println("this is b")
-
-for (i <- 0 until 5) {
-    println(new Random().nextInt)
-}
